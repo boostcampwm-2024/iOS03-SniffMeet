f42ee62dafa81818c2469239f3fe3acba806473f
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-11-07 16:54:26 +0900

    [design] #14 Keywor뷰 생성
    - EdgeInset 설정과 배경 색, cornerRadius 등을 설정하였습니다.

---
+++ SniffMeet/SniffMeet/Source/Common/KeywordView.swift
@@ -0,0 +1,54 @@
+//
+//  KeywordView.swift
+//  SniffMeet
+//
+//  Created by 윤지성 on 11/7/24.
+//
+import UIKit
+
+final class KeywordView: UILabel {
+    private var insets = UIEdgeInsets(
+        top: Context.verticalPadding,
+        left: Context.horizontalPadding,
+        bottom: Context.verticalPadding,
+        right: Context.horizontalPadding
+    )
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(
+            width: size.width + insets.left + insets.right,
+            height: size.height + insets.top + insets.bottom
+        )
+    }
+
+    init(title: String) {
+        super.init(frame: .zero)
+        setupConfiguration(title: title)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func drawText(in rect: CGRect) {
+        let insetRect = rect.inset(by: insets)
+        super.drawText(in: insetRect)
+    }
+
+    private func setupConfiguration(title: String) {
+        text = "#" + title
+        font = UIFont.systemFont(ofSize: 14)
+        layer.cornerRadius = Context.cornerRadius
+        layer.masksToBounds = true
+        self.backgroundColor = UIColor.white
+
+    }
+}
+extension KeywordView {
+    private enum Context {
+        static let verticalPadding: CGFloat = 4
+        static let horizontalPadding: CGFloat = 8
+        static let cornerRadius: CGFloat = 13
+    }
+}
