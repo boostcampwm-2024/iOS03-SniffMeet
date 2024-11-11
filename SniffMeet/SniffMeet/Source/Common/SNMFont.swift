fe350d8da0d62975deeeac129cc1d7a5521f9ea2
Hyemin Heo <heohm128@gmail.com>
2024-11-11 16:54:39 +0900

    [design] #33 SNMFont 추가

---
+++ SniffMeet/SniffMeet/Source/Common/SNMFont.swift
@@ -0,0 +1,29 @@
+//
+//  SNMFont.swift
+//  SniffMeet
+//
+//  Created by sole on 11/11/24.
+//
+
+import UIKit
+
+enum SNMFont {
+    /// size: 34, weight: bold
+    static let largeTitle: UIFont = UIFont.systemFont(ofSize: 34, weight: .bold)
+    /// size: 32,weight: bold
+    static let title1: UIFont = UIFont.systemFont(ofSize: 32, weight: .bold)
+    /// size: 24, weight: heavy
+    static let title2: UIFont = UIFont.systemFont(ofSize: 24, weight: .heavy)
+    /// size: 24,weight: semibold
+    static let title3: UIFont = UIFont.systemFont(ofSize: 24, weight: .semibold)
+    /// size: 16, weight: regular
+    static let body: UIFont = UIFont.systemFont(ofSize: 16, weight: .regular)
+    /// size: 17, weight: semibold
+    static let headline: UIFont = UIFont.systemFont(ofSize: 17, weight: .semibold)
+    /// size: 14, weight: regular
+    static let subheadline: UIFont = UIFont.systemFont(ofSize: 14, weight: .regular)
+    /// size: 16, weight: semibold
+    static let callout: UIFont = UIFont.systemFont(ofSize: 16, weight: .semibold)
+    /// size: 12, weight: regular
+    static let caption: UIFont = UIFont.systemFont(ofSize: 12, weight: .regular)
+}
