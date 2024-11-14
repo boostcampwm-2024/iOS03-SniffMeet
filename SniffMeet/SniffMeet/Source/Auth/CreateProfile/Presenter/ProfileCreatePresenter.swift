b5f680dd8e7fbb7fb378b18c545e6e7835b19593
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-11-14 16:00:26 +0900

    [feat] #49 닉네임 및 프로필 사진 등록 Scene의 Presenter 구현
    해당 Presenter는 ProfileCreatePresentable, DogInfoInteractorOutput 채택하였다.
    - ProfileCreatePresentable 프로토콜은 View가 Presenter를 사용하는 부분
    - DogInfoInteractorOutput 프로토콜은 Interactor가 Presenter를 사용하는 부분

--- SniffMeet/SniffMeet/Source/Auth/CreateProfile/Presenter/ProfileCreatePresenter.swift
+++ SniffMeet/SniffMeet/Source/Auth/CreateProfile/Presenter/ProfileCreatePresenter.swift
@@ -4,21 +4,58 @@
 //
 //  Created by 윤지성 on 11/14/24.
 //
+import Foundation
 
 protocol ProfileCreatePresentable : AnyObject{
+    var dogInfo: DogDetailInfo { get set }
     var view: ProfileCreateViewable? { get set }
     var interactor: ProfileCreateInteractable? { get set }
     var router: ProfileCreateRoutable? { get set }
     
-    func saveDogInfo(newDog: Dog)
+    func saveDogInfo(nickname: String, imageData: Data?)
 }
 
+protocol DogInfoInteractorOutput: AnyObject {
+    func didSaveDogInfo()
+    func didFailToSaveDogInfo(error: Error)
+}
+
+
 final class ProfileCreatePresenter: ProfileCreatePresentable {
+    var dogInfo: DogDetailInfo
     weak var view: ProfileCreateViewable?
     var interactor: ProfileCreateInteractable?
     var router: ProfileCreateRoutable?
     
-    func saveDogInfo(newDog: Dog) {
+    init(dogInfo: DogDetailInfo,
+         view: ProfileCreateViewable? = nil,
+         interactor: ProfileCreateInteractable? = nil,
+         router: ProfileCreateRoutable? = nil)
+    {
+        self.dogInfo = dogInfo
+        self.view = view
+        self.interactor = interactor
+        self.router = router
+    }
+    
+    func saveDogInfo(nickname: String, imageData: Data?) {
+        interactor?.saveDogInfo(dogInfo: Dog(name: dogInfo.name,
+                                             age: dogInfo.age,
+                                             size: dogInfo.size,
+                                             keywords: dogInfo.keywords,
+                                             nickname: nickname,
+                                             profileImage: imageData))
         
+    }
+}
+
+extension ProfileCreatePresenter: DogInfoInteractorOutput {
+    func didSaveDogInfo() {
+        guard let view else { return }
+        router?.presentMainScreen(from: view)
+    }
+    
+    func didFailToSaveDogInfo(error: any Error) {
+        // TODO: -  alert 올리는데 어떻게 올릴지 정하기
     }
 }
