e4c76a0d4fc51193d94181bdaa98d912a36f165e
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-11-14 20:18:02 +0900

    [chore] #52 파일 분리 작업

--- SniffMeet/SniffMeet/Source/Auth/CreateProfile/Interactor/SaveInfoUseCase.swift
+++ SniffMeet/SniffMeet/Source/Auth/CreateProfile/Interactor/SaveInfoUseCase.swift
@@ -6,26 +6,6 @@
 //
 import Foundation
 
-protocol DataStorable {
-    func storeData(data: Encodable) throws
-}
-
-// TODO: -  dataManager를 주입받을 수 있도록 수정 예정
-final class LocalDataManager: DataStorable {
-    private let dataManager = UserDefaultsManager(userDefaults: UserDefaults(suiteName: "demo")!,
-                                                  jsonEncoder: JSONEncoder(),
-                                                  jsonDecoder: JSONDecoder())
-    func storeData(data: any Encodable) throws {
-        try dataManager.set(value: data, forKey: UserDefaultKey.dogInfo)
-    }
-}
-
-extension LocalDataManager {
-    private enum UserDefaultKey {
-        static let dogInfo: String = "dogInfo"
-    }
-}
-
 protocol StoreDogInfoUseCase {
     func execute(dog: Dog) throws
 }
