858af8230adcfe654fa704a05dfcc4e2ac997d0c
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-11-14 11:49:09 +0900

    [feat] #49 Scene에 필요한 엔티티 정의
    1번째 반려견 정보 입력화면과 2번쨰 반려견 정보 입력화면에 필요한 데이터가 달라 엔티티로 따로 구분했다.

---
+++ SniffMeet/SniffMeet/Source/Auth/CreateProfile/Entity/Dog.swift
@@ -0,0 +1,40 @@
+//
+//  DogDTO.swift
+//  SniffMeet
+//
+//  Created by 윤지성 on 11/14/24.
+//
+import Foundation
+
+//. dog(name, age, size, keyword, nickname, image)
+struct DogDetailInfo {
+    let name: String
+    let age: UInt8
+    let size: Size
+    let keywords: [Keyword]
+}
+
+enum Size {
+    case superSmall
+    case small
+    case medium
+    case big
+}
+
+enum Keyword: String  {
+    case energetic = "활발한"
+    case smart = "똑똑한"
+    case active = "적극적인"
+    case shy = "소심한"
+    case independent = "독립적인"
+}
+
+
+struct Dog {
+    let name: String
+    let age: UInt8
+    let size: Size
+    let keywords: [Keyword]
+    let nickname: String
+    let profileImage: Data
+}
