b86a90df0c1619913e03652edd66786e0de2d02f
Hyemin Heo <heohm128@gmail.com>
2024-11-17 05:08:35 +0900

    [test] #56 AnyEncodable 테스트 수정

--- SniffMeet/SNMNetworkTests/AnyEncodableTests.swift
+++ SniffMeet/SNMNetworkTests/AnyEncodableTests.swift
@@ -17,7 +17,12 @@ final class AnyEncodableTests: XCTestCase {
     func test_customEncoder를_주입했을때_인코더_설정대로_인코딩되어야_한다() throws {
         let cusomJSONEncoder = JSONEncoder()
         cusomJSONEncoder.keyEncodingStrategy = .convertToSnakeCase
-        let encodable = AnyEncodable(1, jsonEncoder: cusomJSONEncoder)
-        let _ = try encodable.encode()
+        let encodable = AnyEncodable(MockEncodable(), jsonEncoder: cusomJSONEncoder)
+        let value = try encodable.encode()
+        XCTAssertEqual(#"{"key_case":"value"}"#, String(data: value, encoding: .utf8)!)
+    }
+
+    struct MockEncodable: Encodable {
+        let keyCase: String = "value"
     }
 }
