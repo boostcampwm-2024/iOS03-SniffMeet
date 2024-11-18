e18f9f6fd1f10ee3971353351ebe19cfb9441933
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-11-18 16:38:41 +0900

    [feat] #47 뒤 화면이 보이고 카드 형식으로 presenting되는 커스텀 presentation 방식 구현
    UIPresentationController를 상속하여 커스텀 presentation 방식 구현
    화면를 기준으로 width는 0.9, Height는 0.75로 설정

---
+++ SniffMeet/SniffMeet/Source/Walk/RequestWalk/View/CardPresentationController.swift
@@ -0,0 +1,59 @@
+//
+//  CardPresentationController.swift
+//  SniffMeet
+//
+//  Created by 윤지성 on 11/18/24.
+//
+import UIKit
+
+final class CardPresentationController: UIPresentationController {
+    override var frameOfPresentedViewInContainerView: CGRect {
+        // 중앙에 카드처럼 보이게 할 크기 지정
+        let width: CGFloat = containerView!.bounds.width * 0.9
+        let height: CGFloat = containerView!.bounds.height * 0.75
+        let xValue = (containerView!.bounds.width - width) / 2
+        let yValue = (containerView!.bounds.height - height) / 2
+        return CGRect(x: xValue, y: yValue, width: width, height: height)
+    }
+    
+    override func presentationTransitionWillBegin() {
+        super.presentationTransitionWillBegin()
+        
+        if let containerView = containerView {
+            let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+            blurEffectView.frame = containerView.bounds
+            containerView.addSubview(blurEffectView)
+            blurEffectView.alpha = 0
+            
+            // 애니메이션을 통해 흐림 효과가 나타나도록 설정
+            if let coordinator = presentingViewController.transitionCoordinator {
+                coordinator.animate(alongsideTransition: { context in
+                    blurEffectView.alpha = 0.5
+                })
+            }
+        }
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        super.dismissalTransitionWillBegin()
+        
+        // dismiss 할 때 흐림 효과가 사라지도록 애니메이션 설정
+        if let containerView = containerView {
+            if let blurEffectView = containerView.subviews.first(where: { $0 is UIVisualEffectView }) {
+                if let coordinator = presentingViewController.transitionCoordinator {
+                    coordinator.animate(alongsideTransition: { context in
+                        blurEffectView.alpha = 0
+                    })
+                }
+            }
+        }
+    }
+    
+    override func containerViewWillLayoutSubviews() {
+        super.containerViewWillLayoutSubviews()
+        
+        // 카드 형식의 뷰를 설정
+        presentedView?.layer.cornerRadius = 20
+        presentedView?.clipsToBounds = true
+    }
+}
