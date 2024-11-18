e2f6d80efa548e4d8c657f5afdf1fbb639557476
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-11-18 18:28:44 +0900

    [design] #47 디자인에 맞게 컴포넌트 배치 및 layout constraint 설정

---
+++ SniffMeet/SniffMeet/Source/Walk/RequestWalk/View/RequestWalkViewController.swift
@@ -0,0 +1,147 @@
+//
+//  RequestWalkViewController.swift
+//  SniffMeet
+//
+//  Created by 윤지성 on 11/18/24.
+//
+
+import UIKit
+
+final class RequestWalkViewController: BaseViewController {
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = Context.mainTitle
+        label.font = SNMFont.title3
+        label.textColor = SNMColor.mainNavy
+        return label
+    }()
+    
+    private var mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 15
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage.imagePlaceholder
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "진돌이"
+        label.font = SNMFont.title3
+        label.textColor = SNMColor.mainWhite
+        return label
+    }()
+    
+    private var locationGuideLabel: UILabel = {
+        let label = UILabel()
+        label.text = Context.locationGuideTitle
+        label.font = SNMFont.subheadline
+        label.textColor = SNMColor.mainNavy
+        return label
+    }()
+    
+    private var locationLabel: UILabel = {
+        let label = UILabel()
+        label.font = SNMFont.subheadline
+        label.textColor = SNMColor.subGray2
+        label.text = "잠원 한강 공원"
+        return label
+    }()
+    
+    private var chevronImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.right")
+        imageView.tintColor = SNMColor.subGray2
+        return imageView
+    }()
+    private var locationView: UIView!
+    private var messageTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = Context.requestGuideTitle
+        textView.font = SNMFont.subheadline
+        textView.textColor = SNMColor.black
+        textView.backgroundColor = SNMColor.subGray1
+        textView.layer.cornerRadius = 10
+        return textView
+    }()
+    private var submitButton = PrimaryButton(title: Context.mainTitle)
+    
+    override func configureAttributes() {
+    }
+    override func configureHierachy() {
+        locationView = UIView()
+        [locationGuideLabel, locationLabel, chevronImageView].forEach{
+            locationView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        [titleLabel,
+         mainImageView,
+         nameLabel,
+         locationView,
+         messageTextView,
+         submitButton].forEach{
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    override func configureConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor,
+                                            constant: LayoutConstant.xlargeVerticalPadding),
+            mainImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            mainImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            mainImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
+            mainImageView.heightAnchor.constraint(equalTo: mainImageView.widthAnchor),
+            
+            locationView.topAnchor.constraint(
+                equalTo: mainImageView.bottomAnchor,
+                constant: LayoutConstant.mediumVerticalPadding),
+            locationView.bottomAnchor.constraint(
+                equalTo: messageTextView.topAnchor,
+                constant: -LayoutConstant.regularVerticalPadding),
+            
+            locationGuideLabel.leadingAnchor.constraint(equalTo: locationView.leadingAnchor),
+            locationLabel.trailingAnchor.constraint(equalTo: chevronImageView.leadingAnchor,
+                                                constant: -LayoutConstant.navigationItemSpacing),
+            chevronImageView.trailingAnchor.constraint(equalTo: locationView.trailingAnchor),
+            
+            messageTextView.bottomAnchor.constraint(
+                equalTo: submitButton.topAnchor,
+                constant: -LayoutConstant.xlargeVerticalPadding),
+            
+            submitButton.bottomAnchor.constraint(equalTo: view.bottomAnchor,
+                                                 constant: -LayoutConstant.xlargeVerticalPadding),
+        ])
+        
+        [locationGuideLabel, locationLabel,chevronImageView].forEach{
+            $0.topAnchor.constraint(
+                equalTo: mainImageView.bottomAnchor,
+                constant: LayoutConstant.mediumVerticalPadding).isActive = true
+            $0.bottomAnchor.constraint(
+                equalTo: messageTextView.topAnchor,
+                constant: -LayoutConstant.regularVerticalPadding).isActive = true
+        }
+        
+        [locationView, messageTextView, submitButton].forEach {
+            $0.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: LayoutConstant.smallHorizontalPadding).isActive = true
+            $0.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -LayoutConstant.smallHorizontalPadding).isActive = true
+        }
+    }
+    override func bind() {}
+}
+
+private extension RequestWalkViewController {
+    enum Context {
+        static let mainTitle: String = "산책 요청 보내기"
+        static let locationGuideTitle: String = "장소 선택"
+        static let requestGuideTitle: String = "간단한 요청 메세지를 작성해주세요."
+    }
+}
