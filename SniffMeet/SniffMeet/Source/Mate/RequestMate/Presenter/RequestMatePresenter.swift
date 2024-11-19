5c98fdba1af87c98b629487ed27224af57d9df21
pearhyunjin <pearhyunjin@gmail.com>
2024-11-20 02:36:18 +0900

    [feat] #69 Presenter 구조 구현

---
+++ SniffMeet/SniffMeet/Source/Mate/RequestMate/Presenter/RequestMatePresenter.swift
@@ -0,0 +1,25 @@
+//
+//  RequestMatePresenter.swift
+//  SniffMeet
+//
+//  Created by 배현진 on 11/20/24.
+//
+
+protocol RequestMatePresentable: AnyObject {
+    func didFetchDogProfile(_ dogProfile: DogProfileInfo)
+    func viewDidLoad()
+}
+
+final class RequestMatePresenter: RequestMatePresentable {
+    weak var view: RequestMateViewable?
+    var interactor: RequestMateInteractable?
+    var router: RequestMateRoutable?
+
+    func viewDidLoad() {
+        interactor?.fetchDogProfile()
+    }
+
+    func didFetchDogProfile(_ dogProfile: DogProfileInfo) {
+        view?.updateView(with: dogProfile)
+    }
+}
