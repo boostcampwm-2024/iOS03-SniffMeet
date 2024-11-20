a3dca6811b9789e6acb1f488b9fa4bb30c2eee6a
pearhyunjin <pearhyunjin@gmail.com>
2024-11-20 16:18:46 +0900

    [fix] #69 Buildable프로토콜 추가 구현해서 함수 분리

--- SniffMeet/SniffMeet/Source/Mate/RequestMate/Router/RequestMateRouter.swift
+++ SniffMeet/SniffMeet/Source/Mate/RequestMate/Router/RequestMateRouter.swift
@@ -8,16 +8,28 @@
 import UIKit
 
 protocol RequestMateRoutable: AnyObject {
+    func dismissView(view: any RequestMateViewable)
+}
+
+protocol RequestMateBuildable {
     static func createRequestMateModule() -> UIViewController
 }
 
 final class RequestMateRouter: RequestMateRoutable {
+    func dismissView(view: any RequestMateViewable) {
+        if let view = view as? UIViewController {
+            view.dismiss(animated: true)
+        }
+    }
+}
+
+extension RequestMateRouter: RequestMateBuildable {
     static func createRequestMateModule() -> UIViewController {
         let view = RequestMateViewController()
         let presenter = RequestMatePresenter()
         let interactor = RequestMateInteractor()
-        let router = RequestMateRouter()
-        
+        let router: RequestMateRoutable & RequestMateBuildable = RequestMateRouter()
+
         view.presenter = presenter
         presenter.view = view
         presenter.interactor = interactor
