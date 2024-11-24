f5c087cb523d360b4b3d104e44290bcf6b81c7b4
Hyemin Heo <heohm128@gmail.com>
2024-11-24 20:44:00 +0900

    [add] #89 Notification 엔티티 추가

---
+++ SniffMeet/SniffMeet/Source/Entity/Notification.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.swift
+//  SniffMeet
+//
+//  Created by sole on 11/24/24.
+//
+
+struct Notification {
+    let section: String
+    let description: String
+    let date: String
+}
