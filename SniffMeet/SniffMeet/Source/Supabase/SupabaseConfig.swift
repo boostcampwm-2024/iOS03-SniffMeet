01ee6645ba6c3621b31528681d658fba1230244d
Hyemin Heo <heohm128@gmail.com>
2024-11-24 20:05:44 +0900

    [add] #87 스토리지 버킷 이름 추가

--- SniffMeet/SniffMeet/Source/Supabase/SupabaseConfig.swift
+++ SniffMeet/SniffMeet/Source/Supabase/SupabaseConfig.swift
@@ -10,4 +10,5 @@ import Foundation
 enum SupabaseConfig {
     static let baseURL = URL(string: "https://lltjsznuclppbhxfwslo.supabase.co")!
     static let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsdGpzem51Y2xwcGJoeGZ3c2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0MDA5MjgsImV4cCI6MjA0Njk3NjkyOH0.qrzmB2tiwWevRhKQeptsf5m8iCLUIkscuQ1MzKNtqh4" // 공개키
+    static let bucketName: String = "image"
 }
