4b419daa6ddd8ba6e70f5a32a7c5990ade4ef272
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-11-25 16:30:27 +0900

    [feat] #81 FileManaer를 활용하여 이미지를 get, set, delete하는 함수 구현

--- SniffMeet/SniffMeet/Source/Persistence/SNMFileManager.swift
+++ SniffMeet/SniffMeet/Source/Persistence/SNMFileManager.swift
@@ -12,6 +12,67 @@ protocol ImageManagable {
     func delete(forKey: String) throws
 }
 
+struct SNMFileManager: ImageManagable {
+    private var fileManager: FileManager { FileManager.default }
+    private var documentsDir: URL? {
+        fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+    }
+    
+    func fileExists(forKey path: String) -> Bool {
+        guard let documentsDir else {
+            return false
+        }
+        let fileURL = documentsDir.appendingPathComponent(path, conformingTo: .png)
+        if #available(iOS 16.0, *) {
+            return fileManager.fileExists(atPath: fileURL.path())
+        } else {
+            return fileManager.fileExists(atPath: fileURL.path)
+        }
+    }
+    
+    func get(forKey: String) throws -> UIImage? {
+        guard let documentsDir else {
+            throw FileManagerError.directoryNotFound
+        }
+        let fileURL = documentsDir.appendingPathComponent(forKey, conformingTo: .png)
+        guard fileManager.fileExists(atPath: fileURL.path) else {
+            throw FileManagerError.fileNotFound
+        }
+        return UIImage(contentsOfFile: fileURL.path)
+    }
+    
+    func set(image: UIImage, forKey: String) throws {
+        guard let documentsDir else {
+            throw FileManagerError.directoryNotFound
+        }
+        /// key 값을 받아 해당 키 값이 파일 이름인 파일을 생성한다.
+        let fileURL = documentsDir.appendingPathComponent(forKey, conformingTo: .png)
+       // SNMLogger.info(fileURL.absoluteString)
+        
+        guard let imageData = image.pngData() else {
+            throw FileManagerError.dataConversionError
+        }
+        do {
+            try imageData.write(to: fileURL)
+        } catch {
+            print(error.localizedDescription)
+            throw FileManagerError.writeError
+        }
+    }
+    
+    func delete(forKey: String) throws {
+        guard let documentsDir else {
+            throw FileManagerError.directoryNotFound
+        }
+        let fileURL = documentsDir.appendingPathComponent(forKey, conformingTo: .png)
+        do {
+            try fileManager.removeItem(at: fileURL)
+        } catch {
+            throw FileManagerError.deleteError
+        }
+    }
+}
+
 enum FileManagerError: LocalizedError {
     case directoryNotFound
     case fileNotFound
