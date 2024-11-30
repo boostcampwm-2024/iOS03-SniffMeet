e16645797a5bd4e9aae45ea748079ab6ba811648
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-11-30 20:00:09 +0900

    [feat] #133 notification_list 테이블에 insert DTO와 노티리스트를 요청할 때 필요한 DTO 선언

--- SniffMeet/SniffMeet/Source/DTO/NotiListDTO.swift
+++ SniffMeet/SniffMeet/Source/DTO/NotiListDTO.swift
@@ -6,10 +6,20 @@
 //
 import Foundation
 
-struct NotiListInsertDTO: Encodable {
+/// 회원가입 할 때, 빈 배열로 insert할 때 사용하는 DTO
+struct WalkNotiListInsertDTO: Encodable {
     let id: UUID
     let notifications: [UUID] = []
     enum CodingKeys: String, CodingKey {
         case id, notifications
+    }
+}
+
+/// 노티 리스트를 요청할 때, body에 보내야하는 DTO
+struct WalkNotiListRequestDTO: Encodable {
+    let userId: UUID
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "p_id"
     }
 }
