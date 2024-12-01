8b04c70e9c1d434b9fc991a44eda67f7d58a25ba
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-12-02 00:40:43 +0900

    [feat] #152 debounc, throttle의 실행 간격 시간 상수로 선언

---
+++ SniffMeet/SniffMeet/Source/Common/Design System/EventConstant.swift
@@ -0,0 +1,11 @@
+//
+//  Untitled.swift
+//  SniffMeet
+//
+//  Created by 윤지성 on 12/2/24.
+//
+
+enum EventConstant {
+    static let debounceInterval: Double = 0.5
+    static let throttleInterbal: Double = 0.5
+}
