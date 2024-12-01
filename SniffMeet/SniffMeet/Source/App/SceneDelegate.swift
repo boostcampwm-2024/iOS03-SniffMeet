1669da2d92c106bb2974ae17b801244108f10c70
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-12-01 22:48:56 +0900

    [feat] #150 SessionViewcontroller가 첫 화면을 결정하는 역할을 수행하도록 변경

--- SniffMeet/SniffMeet/Source/App/SceneDelegate.swift
+++ SniffMeet/SniffMeet/Source/App/SceneDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     var appRouter: AppRouter?
+    private weak var sessionController: SessionViewController?
     private let convertToRequestAPS: any ConvertToRequestAPSUseCase = ConverToRequestAPSUseCaseImpl()
     private let convertToResponsdAPS: any ConvertToRespondAPSUseCase = ConvertToRespondAPSUseCaseImpl()
 
@@ -21,26 +22,24 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         appRouter = AppRouter(window: window)
+        let sessionController = SessionViewController(appRouter: appRouter)
+        self.sessionController = sessionController
 
         if let response = connectionOptions.notificationResponse {
             routePushNotification(response: response)
-            return
         }
-        appRouter?.displayInitialScreen()
+        window?.rootViewController = sessionController
+        window?.makeKeyAndVisible()
     }
 
     /// push notification을 통해 앱에 처음 진입한 경우 라우팅을 진행합니다.
     private func routePushNotification(response: UNNotificationResponse) {
         let userInfo = response.notification.request.content.userInfo
         if let requestAPS = convertToRequestAPS.execute(userInfo: userInfo) {
-            appRouter?.initializeViewAndPresentRequestView(
-                walkNoti: requestAPS.walkRequest.toEntity()
-            )
+            sessionController?.walkNoti = requestAPS.walkRequest.toEntity()
         } else if let respondAPS = convertToResponsdAPS.execute(userInfo: userInfo) {
             // TODO: 산책 거절 / 수락 화면으로 라우팅
-            appRouter?.initializeViewAndPresentRespondView(isAccepted: respondAPS.isAccepted)
-        } else {
-            appRouter?.displayInitialScreen()
+            sessionController?.isAccepted = respondAPS.isAccepted
         }
     }
 }
