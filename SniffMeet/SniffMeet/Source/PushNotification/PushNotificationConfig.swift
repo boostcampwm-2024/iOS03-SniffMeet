377be72d42c9f0cd49538a1ae5c7b10d85258db2
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2024-12-02 09:38:13 +0900

    [feat] #161 PushNotificationConfig 선언

---
+++ SniffMeet/SniffMeet/Source/PushNotification/PushNotificationConfig.swift
@@ -0,0 +1,11 @@
+//
+//  PushNotificationConfig.swift
+//  SniffMeet
+//
+//  Created by 윤지성 on 12/2/24.
+//
+import Foundation
+
+public enum PushNotificationConfig {
+    static let baseURL = URL(string: "https://sniff.fly.dev/")!
+}
