58a1c9ef4644fc40adf3f32719ce8f5954af0c22
pearhyunjin <pearhyunjin@gmail.com>
2024-12-04 00:15:43 +0900

    [add] #188 메인 온보딩 화면 추가하고 BaseViewController 연결하기

---
+++ SniffMeet/SniffMeet/Source/OnBoarding/MainBoarding/View/OnBoardingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  OnBoardingViewController.swift
+//  SniffMeet
+//
+//  Created by 배현진 on 12/3/24.
+//
+
+import UIKit
+
+class OnBoardingViewController: BaseViewController {
+    override func configureAttributes() {
+        setButtonActions()
+    }
+    override func configureHierachy() {
+
+    }
+    override func configureConstraints() {
+
+    }
+    override func bind() {
+
+    }
+
+    func setButtonActions() {
+
+    }
+}
