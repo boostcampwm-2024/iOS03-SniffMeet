476f01a39876bac7b6c2010651d0edc28cfbd02e
Hyemin Heo <heohm128@gmail.com>
2024-12-05 02:16:36 +0900

    [feat] #196 ProcessedWalkRouter 구현

---
+++ SniffMeet/SniffMeet/Source/Walk/RespondWalk/Router/ProcessedWalkRouter.swift
@@ -0,0 +1,58 @@
+//
+//  ProcessedWalkRouter.swift
+//  SniffMeet
+//
+//  Created by sole on 12/4/24.
+//
+
+import UIKit
+
+protocol ProcessedWalkRoutable: Routable {
+    func dismiss(view: any ProcessedWalkViewable)
+    func showSelectedLocationMapView(view: any ProcessedWalkViewable, address: Address)
+}
+
+final class ProcessedWalkRouter: ProcessedWalkRoutable {
+    func dismiss(view: any ProcessedWalkViewable) {
+        guard let view = view as? UIViewController else { return }
+        dismiss(from: view, animated: true)
+    }
+    func showSelectedLocationMapView(view: any ProcessedWalkViewable, address: Address) {
+        guard let view = view as? UIViewController else { return }
+        let selectedLocationView = RespondMapRouter.createRespondMapView(address: address)
+        fullScreen(from: view, with: selectedLocationView, animated: true)
+    }
+}
+
+extension ProcessedWalkRouter: ProcessedWalkModuleBuildable {}
+
+// MARK: - ProcessedWalkModuleBuildable
+
+protocol ProcessedWalkModuleBuildable {
+    static func createProcessedWalkView(noti: WalkNoti) -> UIViewController
+}
+
+extension ProcessedWalkModuleBuildable {
+    static func createProcessedWalkView(noti: WalkNoti) -> UIViewController {
+        let view = ProcessedWalkViewController()
+        let presenter = ProcessedWalkPresenter(noti: noti)
+        let interactor = ProcessedWalkInteractor(
+            convertLocationToTextUseCase: ConvertLocationToTextUseCaseImpl(),
+            requestUserInfoUseCase: RequestMateInfoUsecaseImpl(),
+            requestProfileImageUseCase: RequestProfileImageUseCaseImpl(
+                remoteImageManager: SupabaseStorageManager(
+                    networkProvider: SNMNetworkProvider()
+                )
+            )
+        )
+        let router = ProcessedWalkRouter()
+
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.presenter = presenter
+
+        return view
+    }
+}
