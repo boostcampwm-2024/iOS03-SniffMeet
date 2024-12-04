87c1d4f6e6de0d8c9d5b53832543a3303e6f85b0
pearhyunjin <pearhyunjin@gmail.com>
2024-12-04 09:49:50 +0900

    [feat] #188 VIPER 패턴 적용 Interactor, Presenter, Router 구현하고 연결했습니다

---
+++ SniffMeet/SniffMeet/Source/OnBoarding/MainBoarding/Interactor/OnBoardingInteractor.swift
@@ -0,0 +1,35 @@
+//
+//  OnBoardingInteractor.swift
+//  SniffMeet
+//
+//  Created by 배현진 on 12/4/24.
+//
+
+import Foundation
+
+protocol OnBoardingInteractable: AnyObject {
+    var presenter: (any OnBoardingPresentable)? { get set }
+
+    func fetchPages() -> [OnBoardingPage]
+}
+
+final class OnBoardingInteractor: OnBoardingInteractable {
+    weak var presenter: (any OnBoardingPresentable)?
+
+    init(
+        presenter: (any OnBoardingPresentable)? = nil
+    ) {
+        self.presenter = presenter
+    }
+
+    private let pages = [
+        OnBoardingPage(title: "SniffMeet", description: "SniffMeet 앱을 이용하세요", imageName: "placeholder"),
+        OnBoardingPage(title: "프로필 드랍", description: "프로필 드랍을 이용하세요", imageName: "placeholder"),
+        OnBoardingPage(title: "산책 요청", description: "산책 요청을 이용하세요", imageName: "placeholder")
+    ]
+
+    func fetchPages() -> [OnBoardingPage] {
+        SNMLogger.log("interactor fetchPages")
+        return pages
+    }
+}
